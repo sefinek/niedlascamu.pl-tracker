c9fda3d7ec80fcddf6e130cafd72a74d83c0f7cf
Sefinek <contact@sefinek.net>
2024-12-04 16:17:06 +0100

    User-Agent

--- services/axios.js
+++ services/axios.js
@@ -1,8 +1,7 @@
 const axios = require('axios');
-const { name, version, homepage } = require('../package.json');
 
 axios.defaults.headers.common = {
-	'User-Agent': `Mozilla/5.0 (compatible; ${name}/${version}; +${homepage})`,
+	'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36 EdwardGilek/2.0',
 	'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
 	'Accept-Encoding': 'gzip, deflate, br',
 	'Accept-Language': 'pl;q=0.9',
@@ -17,6 +16,6 @@ axios.defaults.headers.common = {
 	'Priority': 'u=0, i'
 };
 
-axios.defaults.timeout = 20000;
+axios.defaults.timeout = 15000;
 
 module.exports = axios;
