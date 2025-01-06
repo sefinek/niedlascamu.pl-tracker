31c3e34fa632af1a957d53eadca7b147ed66d8ce
Sefinek <contact@sefinek.net>
2025-01-06 22:30:38 +0100

    Update BASE_URL

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const path = require('node:path');
 const git = simpleGit();
 
 const WWW_DIR = './www';
-const BASE_URL = ['https://niedlascamu.pl', 'https://banq.niedlascamu.pl', 'https://sklep.niedlascamu.pl'];
+const BASE_URL = ['https://niedlascamu.pl', 'https://banq.niedlascamu.pl'];
 const TRACK_RESOURCES = ['css', 'js', 'jpg', 'jpeg', 'png', 'gif', 'bmp', 'ico', 'pdf'];
 
 const VISITED_URLS = new Set();
