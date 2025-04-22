e0b4dcdc989c287f711222827ea29fbcbb2570aa
Sefinek <contact@sefinek.net>
2025-04-22 22:04:55 +0200

    Fix

--- services/axios.js
+++ services/axios.js
@@ -3,7 +3,6 @@ const axios = require('axios');
 axios.defaults.headers.common = {
 	'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/135.0.0.0 Safari/537.36',
 	'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
-	'Accept-Encoding': 'gzip, deflate, br, zstd',
 	'Accept-Language': 'pl;q=0.9',
 	'Cache-Control': 'max-age=0',
 	'Priority': 'u=0, i',
